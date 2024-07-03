bf824ca4d579c9a3180079a2571f6a5f0f70be56
tuanlda78202 <tuanleducanh78202@gmail.com>
2024-07-04 03:15:09 +0700

    f5

--- src/components/VoiceCircle.tsx
+++
@@ -1,4 +0,0 @@
-export default function VoiceCircle({ transcript }: { transcript: string }) {
-  const key: string = transcript == "" ? transcript : new Date().toString();
-  return <div key={key} className="voice-circle"></div>;
-}
